bd95b9bfef6afc51ba35a3b3365d4e352753c3d5
gadaboinasaikumar521 <gadaboinasaikumar521@gmail.com>
2026-01-05 18:52:53 +0530

    CAtegory

---
+++ src/categories/Category.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Cards from "../cards/Cards";
+
+
+export const Category = () => {
+  const [activeCategory, setActiveCategory] = useState("All");
+
+  const categories = [
+    "All",
+    "Headphones",
+    "Earbuds",
+    "Earphones",
+    "Neckbands"
+  ];
+
+  return (
+    <>
+       <div className="bg-dark">
+      <h2 className="text-center display-5 fw-light  text-white-50 bg-dark">
+        Top Products
+      </h2>
+
+      {/* Category Buttons */}
+      <div className="container mt-4 mb-4">
+        <div className="d-flex flex-wrap justify-content-center gap-3">
+
+          {categories.map(cat => (
+            <button
+              key={cat}
+              onClick={() => setActiveCategory(cat)}
+              className={
+                `btn px-4 py-2 fw-semibold ` +
+                (activeCategory === cat
+                  ? "btn-danger"
+                  : "btn-outline-light")
+              }
+            >
+              {cat}
+            </button>
+          ))}
+
+        </div>
+      </div>
+
+      {/* Products Grid */}
+      <Cards category={activeCategory} />
+       </div>
+    </>
+  );
+};
+
+export default Category;
